26eeb0941d6d5ce89fbc452b414d714906684030
dan_melnikov <dan_tm1994@mail.ru>
2020-01-06 16:48:29 +0300

    Added authentication on Firebase

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,11 +3,14 @@ import classes from './Auth.module.css';
 import Button from '../../components/UI/Button/Button';
 import Input from '../../components/UI/Input/Input';
 import {validateForm} from '../../form/formFramework';
+import axios from 'axios';
 
 function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
+
+const API_KEY = 'AIzaSyCix0VTwuPqMbwrT-062yXFinFsI8zw6UY';
 
 export default class Auth extends React.Component {
     state = {
@@ -40,12 +43,34 @@ export default class Auth extends React.Component {
         }
     };
     
-    loginHandler = () => {
+    loginHandler = async () => {
+        const authData = {
+            email: this.state.formControls.email.value,
+            password: this.state.formControls.password.value,
+            returnSecureToken: true
+        };
 
+        try {
+            const response = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`, authData);            
+            console.log(response.data);
+        } catch (error) {
+            console.log(error);            
+        }
     }
 
-    registerHandler = () => {
+    registerHandler = async () => {
+        const authData = {
+            email: this.state.formControls.email.value,
+            password: this.state.formControls.password.value,
+            returnSecureToken: true
+        };
 
+        try {
+            const response = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`, authData);            
+            console.log(response.data);
+        } catch (error) {
+            console.log(error);            
+        }
     }
 
     submitHandler = (event) => {
